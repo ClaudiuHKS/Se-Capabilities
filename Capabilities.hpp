651afa3d5b7d470185ddd994ab64a2d5227ed115
Hattrick HttrckCldHKS <hattrick.claudiuhks@gmail.com>
2021-06-10 14:02:39 +0300

    Update Capabilities.hpp

--- Capabilities.hpp
+++ Capabilities.hpp
@@ -9,12 +9,12 @@ public:
 
     static constexpr __forceinline const bool hasAVX(void) noexcept
     {
-        return CPUCapabilitiesHandler.m_bs1ECX[::std::atoi(XCS("28"))];
+        return CPUCapabilitiesHandler.m_bs1ECX[SE_28];
     }
 
     static constexpr __forceinline const bool hasAVX2(void) noexcept
     {
-        return CPUCapabilitiesHandler.m_bs7EBX[SE_FIVE];
+        return CPUCapabilitiesHandler.m_bs7EBX[SE_5];
     }
 
     static const CPUCapabilitiesInternal CPUCapabilitiesHandler;
@@ -24,23 +24,23 @@ public:
     public:
 
         __forceinline const CPUCapabilitiesInternal(void) noexcept : \
-            m_nIDs{ SE_ZERO, }, m_nIter{ SE_ZERO, }, m_bs1ECX{ SE_ZERO, }, m_bs7EBX{ SE_ZERO, }, m_vecCPUData{ }, m_arrCPUInfo{ }
+            m_nIDs{ SE_0, }, m_nIter{ SE_0, }, m_bs1ECX{ SE_0, }, m_bs7EBX{ SE_0, }, m_vecCPUData{ }, m_arrCPUInfo{ }
         {
-            ::__cpuid(m_arrCPUInfo.data(), SE_ZERO), m_nIDs = m_arrCPUInfo[SE_ZERO];
+            ::__cpuid(m_arrCPUInfo.data(), SE_0), m_nIDs = m_arrCPUInfo[SE_0];
 
-            for (m_nIter = SE_ZERO; m_nIter < m_nIDs + SE_ONE; ++m_nIter)
-                ::__cpuidex(m_arrCPUInfo.data(), m_nIter, SE_ZERO), m_vecCPUData.emplace_back(m_arrCPUInfo);
+            for (m_nIter = SE_0; m_nIter < (m_nIDs + SE_1); m_nIter = (m_nIter + SE_1))
+                ::__cpuidex(m_arrCPUInfo.data(), m_nIter, SE_0), m_vecCPUData.emplace_back(m_arrCPUInfo);
 
-            if (m_nIDs > SE_ZERO)
-                m_bs1ECX = m_vecCPUData[SE_ONE][SE_TWO];
+            if (m_nIDs > SE_0)
+                m_bs1ECX = m_vecCPUData[SE_1][SE_2];
 
-            if (m_nIDs > SE_SIX)
-                m_bs7EBX = m_vecCPUData[SE_SEVEN][SE_ONE];
+            if (m_nIDs > SE_6)
+                m_bs7EBX = m_vecCPUData[SE_7][SE_1];
         }
 
-        int m_nIDs{ SE_ZERO, }, m_nIter{ SE_ZERO, };
+        int m_nIDs{ SE_0, }, m_nIter{ SE_0, };
 
-        ::std::bitset < 32 > m_bs1ECX{ SE_ZERO, }, m_bs7EBX{ SE_ZERO, };
+        ::std::bitset < 32 > m_bs1ECX{ SE_0, }, m_bs7EBX{ SE_0, };
 
         ::std::vector < ::std::array < int, 4 > > m_vecCPUData{ };
 
