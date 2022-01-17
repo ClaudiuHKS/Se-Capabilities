7f1dd15a024cff1b650d020a951ba2312d300833
Hattrick HttrckCldHKS <hattrick.claudiuhks@gmail.com>
2022-01-17 14:36:51 +0200

    Update Capabilities.hpp

--- Capabilities.hpp
+++ Capabilities.hpp
@@ -13,14 +13,14 @@ public:
 
 public:
 
-    static constexpr __forceinline const bool hasAVX(void) noexcept
+    static constexpr __forceinline const bool hasAVX ( void ) noexcept
     {
-        return CPUCapabilitiesHandler.m_bsAvx[SE_28];
+        return CPUCapabilitiesHandler.m_bsAvx [ SE_28 ];
     }
 
-    static constexpr __forceinline const bool hasAVX2(void) noexcept
+    static constexpr __forceinline const bool hasAVX2 ( void ) noexcept
     {
-        return CPUCapabilitiesHandler.m_bsAvx2[SE_5];
+        return CPUCapabilitiesHandler.m_bsAvx2 [ SE_5 ];
     }
 
 public:
@@ -29,33 +29,33 @@ public:
     {
     public:
 
-        __forceinline const CPUCapabilitiesInternal(void) noexcept : \
-            m_nSize{ SE_0, }, m_nIter{ SE_0, }, m_bsAvx{ SE_0, }, m_bsAvx2{ SE_0, }, m_vecData{ }, m_arrInfo{ }
+        __forceinline const CPUCapabilitiesInternal ( void ) noexcept : \
+            m_nSize { SE_0, }, m_nIter { SE_0, }, m_bsAvx { SE_0, }, m_bsAvx2 { SE_0, }, m_vecData { }, m_arrInfo { }
         {
-            ::__cpuid(m_arrInfo.data(), SE_0), m_nSize = m_arrInfo[SE_0];
+            ::__cpuid ( m_arrInfo.data ( ), SE_0 ), m_nSize = m_arrInfo [ SE_0 ];
 
-            for (m_nIter = SE_0; m_nIter < (m_nSize + SE_1); m_nIter = (m_nIter + SE_1))
-                ::__cpuidex(m_arrInfo.data(), m_nIter, SE_0), m_vecData.emplace_back(m_arrInfo);
+            for ( m_nIter = SE_0; m_nIter < ( m_nSize + SE_1 ); m_nIter = ( m_nIter + SE_1 ) )
+                ::__cpuidex ( m_arrInfo.data ( ), m_nIter, SE_0 ), m_vecData.emplace_back ( m_arrInfo );
 
-            if (m_nSize > SE_0)
+            if ( m_nSize > SE_0 )
             {
-                m_bsAvx = m_vecData[SE_1][SE_2];
+                m_bsAvx = m_vecData [ SE_1 ][ SE_2 ];
 
-                if (m_nSize > SE_6)
-                    m_bsAvx2 = m_vecData[SE_7][SE_1];
+                if ( m_nSize > SE_6 )
+                    m_bsAvx2 = m_vecData [ SE_7 ][ SE_1 ];
             }
         }
 
     public:
 
-        int m_nSize{ SE_0, }, m_nIter{ SE_0, };
+        int m_nSize { SE_0, }, m_nIter { SE_0, };
 
-        ::std::bitset < 32 > m_bsAvx{ SE_0, }, m_bsAvx2{ SE_0, };
+        ::std::bitset < 32 > m_bsAvx { SE_0, }, m_bsAvx2 { SE_0, };
 
-        ::std::vector < ::std::array < int, 4 > > m_vecData{ };
+        ::std::vector < ::std::array < int, 4 > > m_vecData { };
 
-        ::std::array < int, 4 > m_arrInfo{ };
+        ::std::array < int, 4 > m_arrInfo { };
     };
 };
 
-const ::CPUCapabilities::CPUCapabilitiesInternal CPUCapabilities::CPUCapabilitiesHandler{ };
+const ::CPUCapabilities::CPUCapabilitiesInternal CPUCapabilities::CPUCapabilitiesHandler { };
