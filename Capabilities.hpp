3b8c0937fe76e7982649c2667089c0290d181b7a
Hattrick HttrckCldHKS <hattrick.claudiuhks@gmail.com>
2022-09-20 05:05:12 +0300

    Update Capabilities.hpp

--- Capabilities.hpp
+++ Capabilities.hpp
@@ -4,11 +4,8 @@
 #include "Requirements.HPP"
 
 static int cpuCaps() noexcept {
-    int Size{ SE_0, }, Iter{ SE_0, }; ::std::bitset < 32 > Avx1{ }, Avx2{ };
-    ::std::vector < ::std::array < int, 4 > > Data{ }; ::std::array < int, 4 > Info{ };
-    ::__cpuid(Info.data(), SE_0), Size = Info[SE_0];
+    static int Size{ SE_0, }, Iter{ SE_0, }; static ::std::bitset < 32 > Avx1{ }, Avx2{ }; static ::std::vector < ::std::array < int, 4 > > Data{ };
+    static ::std::array < int, 4 > Info{ }; Avx1.reset(), Avx2.reset(), Data.clear(), ::__cpuid(Info.data(), SE_0), Size = Info[SE_0];
     for (Iter = SE_0; Iter < (Size + SE_1); Iter = (Iter + SE_1)) ::__cpuidex(Info.data(), Iter, SE_0), Data.emplace_back(Info);
-    if (Size > SE_0) {
-        Avx1 = Data[SE_1][SE_2]; if (Size > SE_6) Avx2 = Data[SE_7][SE_1];
-    } return Avx2[SE_5] ? SE_2 : (Avx1[SE_28] ? SE_1 : SE_0);
+    if (Size > SE_0) { Avx1 = Data[SE_1][SE_2]; if (Size > SE_6) Avx2 = Data[SE_7][SE_1]; } return Avx2[SE_5] ? SE_2 : (Avx1[SE_28] ? SE_1 : SE_0);
 }
